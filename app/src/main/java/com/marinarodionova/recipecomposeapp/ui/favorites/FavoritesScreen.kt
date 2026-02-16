aa293f6073a02398ee3628454a96fbeb2c9a5554
marina_rodionova <marina_rodionova@>
2026-02-16 16:19:43 +0400

    RCA-21 SharedPreferences

--- app/src/main/java/com/marinarodionova/recipecomposeapp/ui/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/marinarodionova/recipecomposeapp/ui/favorites/FavoritesScreen.kt
@@ -1,23 +1,58 @@
 package com.marinarodionova.recipecomposeapp.ui.favorites
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.marinarodionova.recipecomposeapp.R
+import com.marinarodionova.recipecomposeapp.data.repository.RecipesRepositoryStub
 import com.marinarodionova.recipecomposeapp.ui.core.EmptyPlaceholder
 import com.marinarodionova.recipecomposeapp.ui.core.ScreenHeader
+import com.marinarodionova.recipecomposeapp.ui.recipes.components.RecipeItem
+import com.marinarodionova.recipecomposeapp.ui.recipes.model.toUiModel
+import com.marinarodionova.recipecomposeapp.ui.theme.Dimens
 import com.marinarodionova.recipecomposeapp.ui.theme.RecipeComposeAppTheme
+import com.marinarodionova.recipecomposeapp.utils.FavoritePrefsManager
 
 @Composable
-fun FavoritesScreen(modifier: Modifier = Modifier) {
+fun FavoritesScreen(
+    modifier: Modifier = Modifier, favoritePref: FavoritePrefsManager,
+    onRecipeClick: (Int) -> Unit,
+) {
     Column(modifier = modifier) {
         ScreenHeader(
             stringResource(R.string.title_favorite),
             imageResId = R.drawable.bcg_favorites
         )
-        EmptyPlaceholder(text = stringResource(R.string.information_message_favorite_list))
+        val favoriteIds = favoritePref.getAllFavorites()
+        if (favoriteIds.isEmpty()) {
+            EmptyPlaceholder(text = stringResource(R.string.information_message_favorite_list))
+        } else {
+            val recipes = favoriteIds.map { it.toInt() }
+                .map { id -> RecipesRepositoryStub.getRecipesByRecipeId(id) }
+                .map { it.toUiModel() }
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(1),
+                horizontalArrangement = Arrangement.spacedBy(Dimens.standardMargin),
+                verticalArrangement = Arrangement.spacedBy(Dimens.standardMargin),
+                contentPadding = PaddingValues(Dimens.standardMargin),
+            ) {
+                items(recipes, key = { it.id }) { recipe ->
+                    RecipeItem(
+                        imageUrl = recipe.imageUrl,
+                        title = recipe.title,
+                        onCLick = { onRecipeClick(recipe.id) }
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -25,6 +60,8 @@ fun FavoritesScreen(modifier: Modifier = Modifier) {
 @Composable
 fun FavoritesScreenPreview() {
     RecipeComposeAppTheme {
-        FavoritesScreen()
+        val context = LocalContext.current
+        val favoritePrefsManager = FavoritePrefsManager(context = context)
+        FavoritesScreen(favoritePref = favoritePrefsManager, onRecipeClick = {})
     }
 }
--- app/src/main/java/com/marinarodionova/recipecomposeapp/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/marinarodionova/recipecomposeapp/ui/navigation/AppNavHost.kt
@@ -3,10 +3,7 @@ package com.marinarodionova.recipecomposeapp.ui.navigation
 import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -17,6 +14,7 @@ import com.marinarodionova.recipecomposeapp.ui.categories.CategoriesScreen
 import com.marinarodionova.recipecomposeapp.ui.details.RecipeDetailsScreen
 import com.marinarodionova.recipecomposeapp.ui.favorites.FavoritesScreen
 import com.marinarodionova.recipecomposeapp.ui.recipes.RecipesScreen
+import com.marinarodionova.recipecomposeapp.utils.FavoritePrefsManager
 import kotlinx.coroutines.delay
 
 @Composable
@@ -57,7 +55,13 @@ fun AppNavHost(
         }
 
         composable(route = Destination.Favorites.route) {
-            FavoritesScreen()
+            val context = LocalContext.current
+            val favoritePrefsManager = FavoritePrefsManager(context)
+            FavoritesScreen(favoritePref = favoritePrefsManager, onRecipeClick = { recipeId ->
+                navHostController.navigate(
+                    Destination.RecipeDetails.createRoute(recipeId)
+                )
+            })
         }
 
         composable(
@@ -82,11 +86,12 @@ fun AppNavHost(
             arguments = listOf(navArgument(Constants.PARAM_RECIPE_ID) { type = NavType.IntType })
         ) { backStackEntry ->
             val recipeId = backStackEntry.arguments?.getInt(Constants.PARAM_RECIPE_ID) ?: 0
-            var isFavorite by rememberSaveable { mutableStateOf(false) }
+            val context = LocalContext.current
+            val favoritePrefsManager = FavoritePrefsManager(context)
             RecipeDetailsScreen(
                 recipeId = recipeId,
-                isFavorite = isFavorite,
-                onFavoriteToggle = { isFavorite = !isFavorite })
+                favoritePrefsManager
+            )
         }
     }
 }
