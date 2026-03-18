b583fa3640713d5de7fe4a0cd188e892301b9f3e
marina_rodionova <marina_rodionova@>
2026-03-18 13:12:48 +0400

    RCA-31 Первый сетевой запрос (URLConnection)

--- app/src/main/java/com/marinarodionova/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/marinarodionova/recipecomposeapp/MainActivity.kt
@@ -2,12 +2,18 @@ package com.marinarodionova.recipecomposeapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.marinarodionova.recipecomposeapp.data.model.CategoryDto
+import kotlinx.serialization.json.Json
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.io.bufferedReader
 
 class MainActivity : ComponentActivity() {
     private var deepLinkIntent by mutableStateOf<Intent?>(null)
@@ -20,6 +26,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             RecipesApp(deepLinkIntent = deepLinkIntent)
         }
+
+        Log.i("!!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().getName()}")
+        val thread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.content
+            val categories = connection.inputStream.bufferedReader().readText()
+            connection.disconnect()
+            Log.i("!!!!", "Body: $categories")
+            Log.i("!!!!", "Выполняю запрос на потоке: ${Thread.currentThread().getName()}")
+            val categoryList: List<CategoryDto> = Json.decodeFromString(categories)
+            Log.i(
+                "!!!!",
+                "Кол-во категорий: ${categoryList.size},Название категорий: ${categoryList.map { it.title }} "
+            )
+        }
+        thread.start()
     }
 
     override fun onNewIntent(intent: Intent) {
